f238f3701bc83b9d5b47f69f20d646fddf93ebd8
abhi11september97 <119803800+abhi11september97@users.noreply.github.com>
2023-01-29 22:34:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { connection } = require("./configs/db");
+const cors = require('cors')
 const { UserModel } = require("./models/usersmodel");
 const jwt = require("jsonwebtoken");
 const { iitRouter } = require("./routes/iitRoutes");
@@ -10,6 +11,11 @@ const { upscRouter } = require("./routes/upscRoutes");
 const { sscRouter } = require("./routes/sscRoutes");
 
 const app = express();
+
+app.use(cors({
+  origin: "*"
+}))
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
